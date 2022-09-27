08fd40bb25cf78b2c3152518e85d8a9afce332ac
Farhan Hanif <38966867+Chillrend@users.noreply.github.com>
2022-09-27 09:50:20 +0700

    added candidates migration file

--- database/migrations/2022_09_27_022517_create_candidates_table.php
+++ database/migrations/2022_09_27_022517_create_candidates_table.php
@@ -11,15 +11,10 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('candidates', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,8 +24,8 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('candidates');
     }
 };
