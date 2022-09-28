20679e2ae01f671c274c369e8aab6c9e79526319
Rinaldito <rinalditoryanari@gmail.com>
2022-09-28 07:49:20 +0700

    fix session flash in login

---
+++ resources/views/halaman/login.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
+        name="viewport">
+    <title>Login &mdash; Stisla</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet"
+        href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer" />
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet"
+        href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet"
+        href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('css/components.css') }}">
+</head>
+
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="d-flex align-items-stretch flex-wrap">
+                <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
+                    <div class="m-3 p-4">
+                        <img src="{{ asset('img/pnj.svg') }}"
+                            alt="logo"
+                            width="80"
+                            class="shadow-light rounded-circle mb-5 mt-2">
+                        <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">Evaluasi PMB</span>
+                        </h4>
+                        <p class="text-muted">Before you get started, you must login or register if you don't already
+                            have an account.</p>
+                        @if(session()->has('error'))
+                            <div class="alert alert-danger alert-dismissible show fade">
+                                <div class="alert-body">
+                                    <button class="close"
+                                        data-dismiss="alert">
+                                        <span>&times;</span>
+                                    </button>
+                                    {{session('error')}}
+                                </div>
+                            </div>
+                        @endif
+                        <form method="POST"
+                            action="{{route('actionlogin') }}"
+                            class="needs-validation"
+                            novalidate="">
+                            @csrf <!-- {{ csrf_field() }} -->
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input id="email"
+                                    type="email"
+                                    class="form-control"
+                                    name="email"
+                                    tabindex="1"
+                                    required
+                                    autofocus>
+                                <div class="invalid-feedback">
+                                    Please fill in your email
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="d-block">
+                                    <label for="password"
+                                        class="control-label">Password</label>
+                                </div>
+                                <input id="password"
+                                    type="password"
+                                    class="form-control"
+                                    name="password"
+                                    tabindex="2"
+                                    required>
+                                <div class="invalid-feedback">
+                                    please fill in your password
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox"
+                                        name="remember"
+                                        class="custom-control-input"
+                                        tabindex="3"
+                                        id="remember-me">
+                                    <label class="custom-control-label"
+                                        for="remember-me">Remember Me</label>
+                                </div>
+                            </div>
+
+                            <div class="form-group text-right">
+                                <a href="auth-forgot-password.html"
+                                    class="float-left mt-3">
+                                    Forgot Password?
+                                </a>
+                                <button type="submit"
+                                    class="btn btn-primary btn-lg btn-icon icon-right"
+                                    tabindex="4">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-12 order-lg-2 min-vh-101 position-relative overlay-gradient-bottom "
+                    data-background="{{ asset('img/unsplash/pnj.jpg') }}" style="background-size:108%;">
+                    
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="{{ asset('library/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('library/popper.js/dist/umd/popper.js') }}"></script>
+    <script src="{{ asset('library/tooltip.js/dist/umd/tooltip.js') }}"></script>
+    <script src="{{ asset('library/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('library/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('library/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('js/stisla.js') }}"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="{{ asset('js/scripts.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+</body>
+
+</html>
