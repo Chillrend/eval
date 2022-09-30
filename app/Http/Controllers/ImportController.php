7a4b3bfd23800e0f0aaf57d024b709e8fd3d5f07
Rinaldito <rinalditoryanari@gmail.com>
2022-09-30 14:07:39 +0700

    sudah filtering tinggal insert (masih eror)

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Imports\CandidatesImport;
+use App\Models\Candidates;
 use Maatwebsite\Fascades\Excel;
 
 class ImportController extends Controller
@@ -15,8 +16,31 @@ class ImportController extends Controller
 
     public function import (Request $request) 
     {
-        $test = (new CandidatesImport())->toArray($request->file('excel'));
+        $array = (new CandidatesImport())->toArray($request->file('excel'));
+ 
+        $namedkey = array('nodaftar', 'nama', 'id_pilihan1', 'id_pilihan2', 'id_pilihan3', 'kode_kelompok_bidang', 'alamat', 'sekolah', 'telp');
 
-        dd($test);
+        for ($i=0; $i < count($array[0]); $i++) { 
+            $filtered[] = [
+                'no_daftar'             => $array[0][$i][$namedkey[0]], 
+                'nama'                  => $array[0][$i][$namedkey[1]] , 
+                'id_pilihan1'           => $array[0][$i][$namedkey[2]], 
+                'id_pilihan2'           => $array[0][$i][$namedkey[3]], 
+                'id_pilihan3'           => $array[0][$i][$namedkey[4]], 
+                'kode_kelompok_bidang'  => $array[0][$i][$namedkey[5]], 
+                'alamat'                => $array[0][$i][$namedkey[6]], 
+                'sekolah'               => $array[0][$i][$namedkey[7]], 
+                'telp'                  => $array[0][$i][$namedkey[8]],
+            ] ;
+        }
+
+
+
+
+        // $filtered = (object) $filtered;
+        // $savedd=$filtered->save();
+        $savedd = Candidates::insert($filtered);
+
+        dd($filtered);
     }
 }
