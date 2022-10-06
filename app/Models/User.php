a3ed6fb819566b71439336233a05bb8f9efbc87a
Muhammad Khairudin Ismail <kharisismail56@gmail.com>
2022-10-06 15:53:05 +0700

    Dah bisa tinggal call back multiple table

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Jenssegers\Mongodb\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
 
-class User extends Model
+class User extends Model implements Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use AuthenticatableTrait;
 
     /**
      * The attributes that are mass assignable.
