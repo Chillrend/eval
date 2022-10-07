803ea1147b3e2758139db842dec8084861a5a487
Eka Suci Puspitaningrum <ekaasuci241@gmail.com>
2022-10-07 11:35:03 +0700

    DB jangan lupa diganti

--- app/Models/DynamicImport.php
+++ app/Models/DynamicImport.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Database\Eloquent\Model;
 use Jenssegers\Mongodb\Eloquent\Model;
 
-
 class DynamicImport extends Model
 {
     use HasFactory;
