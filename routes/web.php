cc791cf07253e9c15eccf3850e9e73fff77e329d
Eka Suci Puspitaningrum <ekaasuci241@gmail.com>
2022-10-17 14:54:19 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProdiTesController;
+use App\Http\Controllers\ProdiPresController;
 use App\Http\Controllers\PreviewTesController;
 use App\Http\Controllers\PreviewPresController;
 use App\Http\Controllers\CandidateTesController;
