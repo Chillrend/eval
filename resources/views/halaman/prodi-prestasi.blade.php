375e030d19517681bfeaa1bfa22efb1433192697
Rinaldito <rinalditoryanari@gmail.com>
2022-10-18 14:32:13 +0700

    search bar prodi pres

--- resources/views/halaman/prodi-prestasi.blade.php
+++ resources/views/halaman/prodi-prestasi.blade.php
@@ -110,7 +110,7 @@
                     </div>
                 </div>
             </div>
-            @if($prodi != '')
+            @if($prodi->first() || $searchbar)
                 <h2 class="section-title">Preview</h2>
                 <p class="section-lead">
                     Preview data kuota program studi yang akan di upload
@@ -118,15 +118,43 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
-                            <div class="card-header">
+                            <div class="card-header my-2">
                                 <h4 class="my-2">Tabel Preview Data Kuota Prodi</h4>
+                                @php
+                                $abs = $criteria->first();
+                                @endphp
+                                <div class="card-header-form">
+                                    <form action="/prodi-prestasi" method="get">
+                                        <div class="input-group">
+                                            <select class="form-control" name="kolom" id="periode" onchange="myFunction()">
+                                                <option selected hidden>{{$searchbar[0]  == null ? 'Pilih Kolom' : $searchbar[0]}}</option>
+                                                @foreach($abs['criteria'] as $criteriaa)
+                                                <option>{{$criteriaa}}</option>
+                                                @endforeach
+                                            </select>
+                                            <input type="text" class="form-control" name="search" placeholder="Search" value="{{$searchbar[1]}}">
+                                            <div class="input-group-btn">
+                                                <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>                            
                             </div>
                             <div class="card-body">
-                                <div class="table-responsive">
+                            @if(session()->has('error1'))
+                                <div class="alert alert-danger alert-has-icon alert-dismissible show fade">
+                                    <div class="alert-icon"><i class="fas fa-exclamation"></i></div>
+                                    <div class="alert-body">
+                                        <div class="alert-title">Data Tidak Ditemukan</div>
+                                        {{session('error1')}}
+                                    </div>
+                                    <button class="close" data-dismiss="alert">
+                                        <i class="fas fa-times fa-lg"></i>
+                                    </button>
+                                </div>
+                            @endif
+                            <div class="table-responsive">
                                     <table class="table-hover table display nowrap" id="table" style="width: 100%">
-                                        @php
-                                        $abs = $criteria->first();
-                                        @endphp
                                         <thead>
                                             <tr>
                                                 <th scope="col">#</th>
