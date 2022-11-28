641165d469eb63e013c43fc22c9a95118f89e0b8
Rinaldito <rinalditoryanari@gmail.com>
2022-11-28 21:56:07 +0700

    api prodi mandiri

--- routes/api.php
+++ routes/api.php
@@ -65,6 +65,10 @@ Route::post('/prodi-tes/delete', [ProdiTesController::class, 'api_delete'])->nam
 Route::post('/prodi-tes/edit', [ProdiTesController::class, 'api_edit'])->name('api_editProTes');
 
 //Prodi Mandiri
+Route::get('/prodi-mandiri', [ProdiMandiriController::class, 'api_render'])->name('api_renderPro');
+Route::post('/prodi-mandiri/insert', [ProdiMandiriController::class, 'api_insert'])->name('api_insertPro');
+Route::post('/prodi-mandiri/delete', [ProdiMandiriController::class, 'api_delete'])->name('api_deletePro');
+Route::post('/prodi-mandiri/edit', [ProdiMandiriController::class, 'api_edit'])->name('api_editPro');
 
 
 
