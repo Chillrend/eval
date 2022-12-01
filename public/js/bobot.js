c66d0e410156dbbd6e449b9aa01e1e72f0faba96
Rinaldito <rinalditoryanari@gmail.com>
2022-12-01 15:41:03 +0700

    pembobotan page (mohon benerin lagi gua males)

--- public/js/bobot.js
+++ public/js/bobot.js
@@ -1,229 +1,284 @@
-var criteria = []; 
+var criteria_bobot = [];
+var criteria_prioritas = [];
+var criteria = [];
 var kolom;
 var bobot;
-refresh('','','')
+var toggle = false;
+refresh("", "", "");
 
 function refresh() {
-    var url = document.getElementById("main-content").getAttribute('url');
+    document.getElementById("datatable").setAttribute("hidden", true);
+    var url = document.getElementById("main-content").getAttribute("url");
 
     var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-      
+        method: "GET",
+        redirect: "follow",
+    };
+
     fetch(url, requestOptions)
-    .then(response => response.text())
-    .then(result => {
-        var tahun = JSON.parse(result)
-        console.log(tahun);
-        $("#tahun_terdaftar").empty();
-        let tag ='<option selected hidden disabled>Pilih Tahun</option>'
-        $("#tahun_terdaftar").append(tag);
-        tahun.tahun.forEach(element => {
-            let tag ='<option >'+element+'</option>'
+        .then((response) => response.text())
+        .then((result) => {
+            var tahun = JSON.parse(result);
+            $("#tahun_terdaftar").empty();
+            let tag = "<option selected hidden disabled>Pilih Tahun</option>";
             $("#tahun_terdaftar").append(tag);
-        });
-    })
-    .catch(error => console.log('error', error));
+            tahun.tahun.forEach((element) => {
+                let tag = "<option >" + element + "</option>";
+                $("#tahun_terdaftar").append(tag);
+            });
+        })
+        .catch((error) => console.log("error", error));
 }
 
 function sendTahun() {
-    var url = document.getElementById("tahun_terdaftar").getAttribute('url');
+    var url = document.getElementById("tahun_terdaftar").getAttribute("url");
 
     var formdata = new FormData();
-    formdata.append("tahun", document.getElementById('tahun_terdaftar').value);
-    
+    formdata.append("tahun", document.getElementById("tahun_terdaftar").value);
+
     var requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
     };
-      
+
     fetch(url, requestOptions)
-    .then(response => response.text())
-    .then(result => {
-        var pend = JSON.parse(result)
-        console.log(pend);
+        .then((response) => response.text())
+        .then((result) => {
+            var pend = JSON.parse(result);
 
-        document.getElementById('pendidikan').removeAttribute("disabled");
-        document.getElementById('tahap').setAttribute("disabled", true);
-        document.getElementById('datatable').setAttribute("hidden", true);
+            document.getElementById("pendidikan").removeAttribute("disabled");
+            document.getElementById("tahap").setAttribute("disabled", true);
+            document.getElementById("datatable").setAttribute("hidden", true);
 
-
-        $("#pendidikan").empty();
-        let tag ='<option selected hidden disabled>Pilih Pendidikan</option>'
-        $("#pendidikan").append(tag);
-        pend.pendidikan.forEach(element => {
-            let tag ='<option >'+element+'</option>'
+            $("#pendidikan").empty();
+            let tag =
+                "<option selected hidden disabled>Pilih Pendidikan</option>";
             $("#pendidikan").append(tag);
-        });
-    })
-    .catch(error => console.log('error', error));
+            pend.pendidikan.forEach((element) => {
+                let tag = "<option >" + element + "</option>";
+                $("#pendidikan").append(tag);
+            });
+        })
+        .catch((error) => console.log("error", error));
 }
 
 function sendPend() {
-    var url = document.getElementById("pendidikan").getAttribute('url');
+    var url = document.getElementById("pendidikan").getAttribute("url");
 
     var formdata = new FormData();
-    formdata.append("tahun", document.getElementById('tahun_terdaftar').value);
-    formdata.append("pendidikan", document.getElementById('pendidikan').value);
-    
+    formdata.append("tahun", document.getElementById("tahun_terdaftar").value);
+    formdata.append("pendidikan", document.getElementById("pendidikan").value);
+
     var requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
     };
-      
+
     fetch(url, requestOptions)
-    .then(response => response.text())
-    .then(result => {
-        var tahap = JSON.parse(result)
-        console.log(tahap.tahap);
+        .then((response) => response.text())
+        .then((result) => {
+            var tahap = JSON.parse(result);
 
-        document.getElementById('tahap').removeAttribute("disabled");
-        document.getElementById('datatable').setAttribute("hidden", true);
+            document.getElementById("tahap").removeAttribute("disabled");
+            document.getElementById("datatable").setAttribute("hidden", true);
 
-
-        $("#tahap").empty();
-        let tag ='<option selected hidden disabled>Pilih Tahap</option>'
-        $("#tahap").append(tag);
-        tahap.tahap.forEach(element => {
-            switch (element) {
-                case 'candidates_tes':
-                    tag ='<option value="'+element+'" >Tes</option>'
-                    break;
-                case 'candidates_pres':
-                    tag ='<option value="'+element+'" >Prestasi</option>'
-                    break;
-                case 'candidates_mand':
-                    tag ='<option value="'+element+'" >Mandiri</option>'
-                    break;
-            }
+            $("#tahap").empty();
+            let tag = "<option selected hidden disabled>Pilih Tahap</option>";
             $("#tahap").append(tag);
-        });
-    })
-    .catch(error => console.log('error', error));
+            tahap.tahap.forEach((element) => {
+                switch (element) {
+                    case "candidates_tes":
+                        tag = '<option value="' + element + '" >Tes</option>';
+                        break;
+                    case "candidates_pres":
+                        tag =
+                            '<option value="' +
+                            element +
+                            '" >Prestasi</option>';
+                        break;
+                    case "candidates_mand":
+                        tag =
+                            '<option value="' + element + '" >Mandiri</option>';
+                        break;
+                }
+                $("#tahap").append(tag);
+            });
+        })
+        .catch((error) => console.log("error", error));
 }
 
 function sendTahap() {
-    var url = document.getElementById("tahap").getAttribute('url');
+    table();
+    document.getElementById("datatable").removeAttribute("hidden");
 
-    var formdata = new FormData();
-    formdata.append("tahun", document.getElementById('tahun_terdaftar').value);
-    formdata.append("pendidikan", document.getElementById('pendidikan').value);
-    formdata.append("tahap", document.getElementById('tahap').value);
-    
-    var requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
-    };
-    
-    fetch(url, requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            dataAPI = JSON.parse(result)
-            kolom = dataAPI.criteria;
-            console.log(kolom);
+    document.getElementById("nilai").setAttribute("disabled", true);
+    document.getElementById("bobot").setAttribute("disabled", true);
+    document.getElementById("prioritas").setAttribute("disabled", true);
+}
 
-            document.getElementById('datatable').removeAttribute("hidden");
+function table() {
+    $("#table-responsive").empty();
 
-            document.getElementById('nilai').setAttribute("disabled", true);
-            document.getElementById('bobot').setAttribute("disabled", true);
-            document.getElementById('prioritas').setAttribute("disabled", true);
-            
-            
-            $("#kolom").empty();
-            let tag ='<option selected hidden>Pilih Kolom</option>'
-            $("#kolom").append(tag);
-            kolom.forEach(element => {
-                let tag ='<option >'+element+'</option>'
-                $("#kolom").append(tag);
-            });        
-        })
-      .catch(error => console.log('error', error));
+    let tag =
+        '<table class="table-hover table-md table display nowrap" id="tbl-bobot">';
+    tag += "<thead>";
+    tag += "<tr>";
+    tag += '<th scope="col">#</i></th>';
+    tag += '<th scope="col">Kolom</th>';
+    tag += '<th scope="col">Nilai</th>';
+    tag += '<th scope="col"><i class="fas fa-star"></i></th>';
+    tag += '<th scope="col">Action</th>';
+    tag += "</tr>";
+    tag += "</thead>";
+    tag += '<tbody id="table-body">';
+    tag += "</tbody>";
+    tag += "</table>";
+    $("#table-responsive").append(tag);
+
+    for (let index = 0; index < criteria.length; index++) {
+        let tag = null;
+        tag += "<tr>";
+        tag += "<td>" + (index + 1) + "</td>";
+        tag += "<td>" + criteria[index][0] + "</td>";
+        tag += "<td>" + criteria[index][1] + "</td>";
+        if (criteria[index][3] == true) {
+            tag += "<td><code>Prioritas</code></td>";
+        } else {
+            tag += "<td>" + criteria[index][2] + "</td>";
+        }
+        tag += "<td>1</td>";
+        tag += "</tr>";
+        $("#table-body").append(tag);
+    }
+
+    $("#tbl-bobot").DataTable({
+        scrollX: true,
+        responsive: false,
+        pageLength: 10,
+        autoWidth: false,
+    });
 }
 
 function sendKolom() {
-    var url = document.getElementById("kolom").getAttribute('url');
+    var url = document.getElementById("kolom").getAttribute("url");
 
     var formdata = new FormData();
-    formdata.append("tahun", document.getElementById('tahun_terdaftar').value);
-    formdata.append("pendidikan", document.getElementById('pendidikan').value);
-    formdata.append("tahap", document.getElementById('tahap').value);
-    formdata.append("kolom", document.getElementById('kolom').value);
-    
-    var requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
-    };
-    
-    fetch(url, requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            dataAPI = JSON.parse(result)
-            nilai = dataAPI.nilai;
-            console.log(nilai);
+    formdata.append("tahun", document.getElementById("tahun_terdaftar").value);
+    formdata.append("pendidikan", document.getElementById("pendidikan").value);
+    formdata.append("tahap", document.getElementById("tahap").value);
+    formdata.append("kolom", document.getElementById("kolom").value);
 
-            document.getElementById('datatable').removeAttribute("hidden");
-            document.getElementById('nilai').removeAttribute("disabled");
-            document.getElementById('bobot').setAttribute("disabled", true);
-            document.getElementById('prioritas').setAttribute("disabled", true);
-            
+    var requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
+    };
+
+    fetch(url, requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            dataAPI = JSON.parse(result);
+            nilai = dataAPI.nilai;
+
+            document.getElementById("datatable").removeAttribute("hidden");
+            document.getElementById("nilai").removeAttribute("disabled");
+            document.getElementById("bobot").setAttribute("disabled", true);
+            document.getElementById("prioritas").setAttribute("disabled", true);
+
             $("#nilai").empty();
-            let tag ='<option selected hidden>Pilih Nilai</option>'
+            let tag = "<option selected hidden>Pilih Nilai</option>";
             $("#nilai").append(tag);
-            nilai.forEach(element => {
-                let tag ='<option >'+element+'</option>'
+            nilai.forEach((element) => {
+                let tag = "<option >" + element + "</option>";
                 $("#nilai").append(tag);
-            });        
+            });
         })
-        .catch(error => console.log('error', error));
+        .catch((error) => console.log("error", error));
 }
 
 function sendNilai() {
-    document.getElementById('bobot').removeAttribute("disabled");
-    document.getElementById('prioritas').removeAttribute("disabled");
+    document.getElementById("bobot").removeAttribute("disabled");
+    document.getElementById("prioritas").removeAttribute("disabled");
 }
-
-var toggle = false
 
 function setPrioritas() {
     if (toggle == false) {
-        document.getElementById('bobot').setAttribute("disabled", true);
+        document.getElementById("bobot").setAttribute("disabled", true);
         toggle = true;
     } else {
-        document.getElementById('bobot').removeAttribute("disabled");
+        document.getElementById("bobot").removeAttribute("disabled");
         toggle = false;
     }
 }
 
 function showForm() {
-    document.getElementById('form-bobot').removeAttribute('hidden');
-    document.getElementById('form-bobot').focus();
-        
+    var url = document.getElementById("tahap").getAttribute("url");
+
+    var formdata = new FormData();
+    formdata.append("tahun", document.getElementById("tahun_terdaftar").value);
+    formdata.append("pendidikan", document.getElementById("pendidikan").value);
+    formdata.append("tahap", document.getElementById("tahap").value);
+
+    var requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
+    };
+
+    fetch(url, requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            dataAPI = JSON.parse(result);
+            kolom = dataAPI.criteria;
+
+            $("#kolom").empty();
+            let tag = "<option selected hidden>Pilih Kolom</option>";
+            $("#kolom").append(tag);
+            kolom.forEach((element) => {
+                let tag = "<option >" + element + "</option>";
+                $("#kolom").append(tag);
+            });
+            document.getElementById("form-bobot").removeAttribute("hidden");
+            document.getElementById("form-bobot").focus();
+        })
+        .catch((error) => console.log("error", error));
 }
 
-function tutup(){
-
+function tutup() {
     $("#kolom").empty();
-    let tag1 ='<option selected hidden>Pilih Kolom</option>'
+    let tag1 = "<option selected hidden>Pilih Kolom</option>";
     $("#kolom").append(tag1);
 
     $("#nilai").empty();
-    let tag ='<option selected hidden>Pilih Nilai</option>'
+    let tag = "<option selected hidden>Pilih Nilai</option>";
     $("#nilai").append(tag);
 
-    document.getElementById('nilai').value = 0;
+    document.getElementById("nilai").value = 0;
 
+    document.getElementById("datatable").removeAttribute("hidden");
+    document.getElementById("nilai").setAttribute("disabled", true);
+    document.getElementById("bobot").setAttribute("disabled", true);
+    document.getElementById("prioritas").setAttribute("disabled", true);
 
-    document.getElementById('datatable').removeAttribute("hidden");
-    document.getElementById('nilai').setAttribute("disabled", true);
-    document.getElementById('bobot').setAttribute("disabled", true);
-    document.getElementById('prioritas').setAttribute("disabled", true);
+    document.getElementById("form-bobot").setAttribute("hidden", true);
+}
 
-    document.getElementById('binding').setAttribute('hidden',true);
-    sessionStorage.clear();
-  }
-  
+function addCriteria() {
+    var kolom = document.getElementById("kolom").value;
+    var nilai = document.getElementById("nilai").value;
+    var bobot = document.getElementById("bobot").value;
+    var priotity = toggle;
+    // criteria.push([kolom, nilai, bobot, priotity]);
+    // console.log(criteria);
+
+    if (priotity == true) {
+        criteria_prioritas.push([kolom, nilai, bobot, priotity]);
+    } else {
+        criteria_bobot.push([kolom, nilai, bobot, priotity]);
+    }
+    criteria = criteria_prioritas.concat(criteria_bobot);
+    tutup();
+    table();
+}
--- resources/views/halaman/pembobotan.blade.php
+++ resources/views/halaman/pembobotan.blade.php
@@ -4,10 +4,9 @@
 
 @push('style')
 <!-- CSS Libraries -->
-<link rel="stylesheet"
-        href="{{ asset('library/prismjs/themes/prism.min.css') }}">
-<link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
 <link rel="stylesheet" href="{{ asset('library/datatables/media/css/jquery.dataTables.min.css') }}">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css" />
+<link rel="stylesheet" href="https://cdn.datatables.net/select/1.3.3/css/select.bootstrap4.min.css" />
 
 @endpush
 
@@ -22,32 +21,38 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header d-flex justify-content-between">
-                        
-                        <div class="d-flex align-items-center col-12 col-xl-3 col-lg-4 col-md-4 col-sm-12 my-1">
-                            <label class="my-0" for="tahun_terdaftar"><h6 class="my-0 mr-3">Tahun</h6></label>
-                            <select class="form-control" name="tahun_terdaftar" id="tahun_terdaftar" onchange="sendTahun()" url="{{route('api_pendBobot')}}">
-                                <option selected hidden disabled>Pilih tahun</option>
-                            </select>
-                        </div>
-                        
-                        <div class="d-flex align-items-center col-12 col-xl-3 col-lg-4 col-md-4 col-sm-12 my-1">
-                            <label class="my-0" for="pendidikan"><h6 class="my-0 mr-3">Pendidikan</h6></label>
-                            <select class="form-control" name="pendidikan" id="pendidikan" onchange="sendPend()" url="{{route('api_tahapBobot')}}" disabled>
-                                <option selected hidden disabled>Pilih Pendidikan</option>
-                            </select>
-                        </div>    
 
-                        <div class="d-flex align-items-center col-12 col-xl-3 col-lg-4 col-md-4 col-sm-12 my-1">
-                            <label class="my-0" for="tahap"><h6 class="my-0 mr-3">Tahap</h6></label>
-                            <select class="form-control" name="tahap" id="tahap" onchange="sendTahap()" url="{{route('api_dataBobot')}}" disabled>
-                                <option selected hidden disabled>Pilih Tahap</option>
-                            </select>
-                        </div>
+                            <div class="d-flex align-items-center col-12 col-xl-3 col-lg-4 col-md-4 col-sm-12 my-1">
+                                <label class="my-0" for="tahun_terdaftar">
+                                    <h6 class="my-0 mr-3">Tahun</h6>
+                                </label>
+                                <select class="form-control" name="tahun_terdaftar" id="tahun_terdaftar" onchange="sendTahun()" url="{{route('api_pendBobot')}}">
+                                    <option selected hidden disabled>Pilih tahun</option>
+                                </select>
+                            </div>
+
+                            <div class="d-flex align-items-center col-12 col-xl-3 col-lg-4 col-md-4 col-sm-12 my-1">
+                                <label class="my-0" for="pendidikan">
+                                    <h6 class="my-0 mr-3">Pendidikan</h6>
+                                </label>
+                                <select class="form-control" name="pendidikan" id="pendidikan" onchange="sendPend()" url="{{route('api_tahapBobot')}}" disabled>
+                                    <option selected hidden disabled>Pilih Pendidikan</option>
+                                </select>
+                            </div>
+
+                            <div class="d-flex align-items-center col-12 col-xl-3 col-lg-4 col-md-4 col-sm-12 my-1">
+                                <label class="my-0" for="tahap">
+                                    <h6 class="my-0 mr-3">Tahap</h6>
+                                </label>
+                                <select class="form-control" name="tahap" id="tahap" onchange="sendTahap()" url="{{route('api_dataBobot')}}" disabled>
+                                    <option selected hidden disabled>Pilih Tahap</option>
+                                </select>
+                            </div>
 
                         </div>
                     </div>
 
-                    <div class="card" id="datatable" hidden>
+                    <div class="card" id="datatable">
                         <div class="card-body">
                             <div class="d-flex justify-content-between">
                                 <div class="align-items-left">
@@ -56,47 +61,9 @@
                             </div>
                             <br>
 
-                            @if (session()->has('success'))
-                                <div class="alert alert-success alert-dismissible show fade">
-                                    <div class="alert-body">
-                                        <button class="close"
-                                            data-dismiss="alert">
-                                            <span>&times;</span>
-                                        </button>
-                                        {{ session('success') }}
-                                    </div>
-                                </div>
-                            @endif
-
-                            @if ($errors->any())
-                                @foreach ($errors->all() as $error)
-                                <div class="alert alert-danger alert-dismissible show fade">
-                                    <div class="alert-body">
-                                        <button class="close"
-                                            data-dismiss="alert">
-                                            <span>&times;</span>
-                                        </button>
-                                        {{ $error }}
-                                    </div>
-                                </div>
-                                @endforeach
-                            @endif
-
                             <br>
-                            <div class="">
-                                <table class="table-hover table-md table display nowrap" id="tbl-bobot">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">#</i></th>
-                                            <th scope="col">Kolom</th>
-                                            <th scope="col">Nilai</th>
-                                            <th scope="col"><i class="fas fa-star"></i></th>
-                                            <th scope="col">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    </tbody>
-                                </table>
+                            <div class="table-responsive" id="table-responsive">
+
                             </div>
                         </div>
                         <div class="card-footer text-right">
@@ -109,7 +76,7 @@
     </section>
 
 
-    
+
     <section id="form-bobot" hidden>
         <div class="row">
             <div class="col-12">
@@ -132,7 +99,7 @@
                                 <div class="col-sm-12 col-md-7">
                                     <select type="number" class="form-control" name="nilai" id="nilai" onchange="sendNilai()" required disabled>
                                         <option selected hidden>Pilih Nilai</option>
-                                    </select>       
+                                    </select>
                                 </div>
                             </div>
                             <div class="form-group row mb-4">
@@ -144,10 +111,7 @@
                             <div class="form-group row mb-4 d-flex">
                                 <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
                                 <label class="custom-switch col-sm-12 col-md-7 px-3">
-                                    <input type="checkbox"
-                                        name="prioritas" id="prioritas"
-                                        name="custom-switch-checkbox"
-                                        class="custom-switch-input" onclick="setPrioritas()">
+                                    <input type="checkbox" name="prioritas" id="prioritas" name="custom-switch-checkbox" class="custom-switch-input" onclick="setPrioritas()">
                                     <span class="custom-switch-indicator"></span>
                                     <span class="custom-switch-description">Criteria ini akan dipriotaskan tanpa pembobotan</span>
                                 </label>
@@ -155,8 +119,8 @@
                             <div class="form-group row mb-4">
                                 <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
                                 <div class="col-sm-12 col-md-7">
-                                    <button class="btn btn-success" type="submit">Submit</button>
-                                    <input class="btn btn-danger ml-2"type="button" value="Close" onclick="tutup()">
+                                    <button class="btn btn-success" type="button" onclick="addCriteria()">Submit</button>
+                                    <button class="btn btn-danger ml-2" type="button" onclick="tutup()">Close</button>
                                 </div>
                             </div>
                         </form>
@@ -171,20 +135,14 @@
 
 @push('scripts')
 <!-- JS Libraies -->
-<script src="{{ asset('library/prismjs/prism.js') }}"></script>
-<script src="assets/modules/datatables/Select-1.2.4/js/dataTables.select.min.js"></script>
 <script src="{{ asset('library/datatables/media/js/jquery.dataTables.min.js') }}"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.3.3/js/select.bootstrap4.js"></script>
 <script src="{{ asset('library/jquery-ui-dist/jquery-ui.min.js') }}"></script>
 <script src="{{ asset('library/sweetalert/dist/sweetalert.min.js') }}"></script>
-<script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
 <script src="{{ asset('js/stisla.js') }}"></script>
 <script src="{{ asset('library/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-
 <!-- Page Specific JS File -->
 <script src="../../js/bobot.js"></script>
-<script src="../../js/table.js"></script>
 <script src="../../js/style.js"></script>
-<!-- <script src="../../js/prodi.js"></script> -->
-<script src="{{ asset('js/page/modules-sweetalert.js') }}"></script>
-
 @endpush
